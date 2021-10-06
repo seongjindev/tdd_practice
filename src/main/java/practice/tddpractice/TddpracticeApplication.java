d05a774619828b34e6d3efec2d80fd8f97c32446
bae <baesj11@gmail.com>
2021-10-06 18:18:32 +0900

    first commit
    스프링부트 실행 확인

---
+++ src/main/java/practice/tddpractice/TddpracticeApplication.java
@@ -0,0 +1,13 @@
+package practice.tddpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddpracticeApplication.class, args);
+	}
+
+}
