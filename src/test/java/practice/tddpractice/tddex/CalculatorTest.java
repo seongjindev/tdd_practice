fe3e0108197267aab69208491c78ab983f401229
bae <baesj11@gmail.com>
2021-10-07 18:45:01 +0900

    Calculator - subtract
    빼기 메서드 만들기

--- src/test/java/practice/tddpractice/tddex/CalculatorTest.java
+++ src/test/java/practice/tddpractice/tddex/CalculatorTest.java
@@ -27,4 +27,27 @@ public class CalculatorTest {
         Assertions.assertThat(result3).isEqualTo(115);
         Assertions.assertThat(result4).isEqualTo(3);
     }
+
+    @Test
+    void 빼기() {
+        //given
+        int a = 10;
+        int b = 5;
+        int a2 = 20;
+        int b2 = 30;
+        int a3 = 200;
+        int b3 = 100;
+        int a4 = 1000;
+        int b4 = 3500;
+        //when
+        int result = Calculator.subtract(a, b);
+        int result2 = Calculator.subtract(a2, b2);
+        int result3 = Calculator.subtract(a3, b3);
+        int result4 = Calculator.subtract(a4, b4);
+        //then
+        Assertions.assertThat(result).isEqualTo(5);
+        Assertions.assertThat(result2).isEqualTo(-10);
+        Assertions.assertThat(result3).isEqualTo(100);
+        Assertions.assertThat(result4).isEqualTo(-2500);
+    }
 }
